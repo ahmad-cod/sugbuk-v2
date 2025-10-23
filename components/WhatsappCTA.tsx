1806513f15b9b7ccb41c0d74ce30ee65689b0319
ahmad-cod <ahmadadewale800@gmail.com>
2025-10-23 11:55:12 +0100

    feat: Add CTA for joining whatsapp channel

---
+++ components/WhatsappCTA.tsx
@@ -0,0 +1,16 @@
+import { whatsappChannel } from '@/lib/constants';
+import { MessageCircle, ChevronRight } from 'lucide-react';
+
+export const WhatsappChannel = () => {
+  return (
+    <button 
+      onClick={() => window.open(whatsappChannel, '_blank')}
+      className="group bg-white hover:bg-blue-100 text-blue-600 px-8 py-4 rounded-full font-semibold text-lg transition-all duration-300 transform hover:cursor-pointer hover:shadow-2xl inline-flex items-center space-x-2"
+    >
+      <MessageCircle className="w-5 h-5" />
+      <span>Join Our WhatsApp Channel</span>
+      <ChevronRight className="w-5 h-5 group-hover:translate-x-1 animate-pulse transition-transform" />
+    </button>
+
+  )
+}
