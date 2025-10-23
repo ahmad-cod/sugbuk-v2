950aad889a758bd0b9530a1a4b0f0fe9df9b036f
ahmad-cod <ahmadadewale800@gmail.com>
2025-10-23 12:44:02 +0100

    refactor: Remove commented-out HeroSection import and usage in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import { AboutSection } from "@/components/AboutSection";
 import CTA from "@/components/cta";
 import Hero from "@/components/hero";
-// import { HeroSection } from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <section className="flex flex-col gap-y-20 w-full">
       <Hero />
-      {/* <HeroSection /> */}
       <AboutSection />
       <CTA />
     </section>
