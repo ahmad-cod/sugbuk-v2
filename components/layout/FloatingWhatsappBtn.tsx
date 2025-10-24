d475b8b06f5743536c89fe1aa5e659eeec7dd519
ahmad-cod <ahmadadewale800@gmail.com>
2025-10-24 18:04:12 +0100

    fix: update Floating WhatsApp icon colors for better visibility

--- components/layout/FloatingWhatsappBtn.tsx
+++ components/layout/FloatingWhatsappBtn.tsx
@@ -5,9 +5,9 @@ const FloatingWhatsappIcon = () => {
   return (
     <a
     href={whatsappChannel}
-    className="fixed bottom-5 right-3 z-50 bg-white sm:bg-green-500 hover:bg-green-600 text-white px-3 sm:px-5 py-3 rounded-full shadow-lg flex items-center gap-2"
+    className="fixed bottom-5 right-3 z-50 sm:bg-green-500 hover:bg-green-600 text-white px-3 sm:px-5 py-3 rounded-full shadow-lg flex items-center gap-2"
 >
-  <Whatsapp className="w-8 h-8 text-green-500 sm:text-white animate-[bounce_3s_ease-in_infinite]" />
+  <Whatsapp className="w-8 h-8 text-green-600 sm:text-white animate-[bounce_3s_ease-in_infinite]" />
   <span className="hidden sm:inline font-medium">Join Channel</span>
 </a>
   )
